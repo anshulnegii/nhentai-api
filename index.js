e12d86265ea5c90f6677da5ff034dc9fe01ac600
anshulnegii <nanshul2003@gmail.com>
2022-11-23 15:25:36 +0530

    changed function name

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
-//const PORT = 3000;
 
 const app = express();
 app.use(express.json())
 
-async function getDoujin(doujin) {
+async function getChapters(doujin) {
     try {
         const siteURL = `https://nhentai.to/g/${doujin}`
         const { data } = await axios({
@@ -29,6 +28,7 @@ async function getDoujin(doujin) {
     }
 }
 
+
 app.get('/', async (req, res) =>{
     res.send('nhentai api 🌚')
 })
@@ -41,12 +41,11 @@ app.get('/api', async (req, res) =>{
 
 app.get('/api/:id', async  (req, res) => {
     const { id } = req.params
-    var a = await getDoujin(id)
+    var a = await getChapters(id)
     res.send({chapters: a})
 })
 
 
 app.listen(
     process.env.PORT || 3000,
-    // console.log(`http://localhost:${PORT}`)
 )
